5495921b333ff86e4761130e7255b6ea1a130201
Ikerm01 <ikermacias10@gmail.com>
2024-02-08 08:30:27 +0100

    Terminada la segunda funcion para calcular la media, he usado el .split para solucionar el error que de conversion que me daba

--- observatori.py
+++ observatori.py
@@ -1,4 +1,4 @@
-//Pre: Hacer un programa que regriste la temperatura semanal para un observatorio
+#Pre: Hacer un programa que regriste la temperatura semanal para un observatorio
 temperatura_semanal = []
 fecha_acutal = []
 
@@ -6,20 +6,18 @@ def regristar_temperatura_setmanal(temperatura_semanal):
     temperaturas = input("Escriu les temperatures d'aquesta setmana")
     temperatura_semanal.append(temperaturas)
 def consultar_temperatura_mitjana():
-    def consultar_temperatura_mitjana():
-    for temperatura in temperatura_semanal:
+    for temperaturas in temperatura_semanal:
         suma = 0
-        for suma in temperatura:
-            suma += temperatura
-        media = suma /len(temperatura)
+        media_temperatura = temperaturas.split(',')
+        for temperatura in media_temperatura:
+            suma += int(temperatura)
+        media = suma /len(temperaturas)
         media_temperatura.append(media)
         print(media_temperatura)
-def consultar_diferencia_maxima():
+#def consultar_diferencia_maxima():
 
+opcion = 0
 
-
-
-opcion = "0"
 while opcion != "FI":
     print("[RT]. Registrar temperatures setmanals.")
     print("[MJ]. Consultar temperatura mitjana. ")
@@ -29,7 +27,7 @@ while opcion != "FI":
     opcion = input("Seleccione una opción: ")
 
     if opcion == "RT":
-        regristar_temperatura_setmanal()
+        regristar_temperatura_setmanal(temperatura_semanal)
     elif opcion == "MJ":
         consultar_temperatura_mitjana()
     elif opcion == "DF":
@@ -37,4 +35,4 @@ while opcion != "FI":
     elif opcion == "FI":
         print("Saliendo del programa...")
         break
-//Post:Poder consultar en cualquier momento cuál ha sido la temperatura media y la diferencia entre el valor máximo y mínimo registrados y la fecha actual también se muestra en pantalla.
+#Post:Poder consultar en cualquier momento cuál ha sido la temperatura media y la diferencia entre el valor máximo y mínimo registrados y la fecha actual también se muestra en pantalla.

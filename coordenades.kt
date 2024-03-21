2f9ba90f54e72cfc902490cb63a5363fe91464ab
Ikerm01 <ikermacias10@gmail.com>
2024-03-21 11:39:30 +0100

    si

---
+++ coordenades.kt
@@ -0,0 +1,14 @@
+class Coordenada{
+    constructor(){
+        self.x: int = 0
+        self.y: int = 0
+    fun moure_dreta(self) -> None:
+        self.x += 1
+    fun moure_dreta(self) -> None:
+        self.x -= 1
+    fun moure_dreta(self) -> None:
+        self.x += 1
+    fun moure_dreta(self) -> None:
+        self.x -= 1
+    }
+}
---
+++ prestamo.py
@@ -0,0 +1,18 @@
+from libro import Libro
+#from socio import Socio
+
+class Prestamo:
+    def __init__(self, libro: Libro,): #socio: Socio):
+        self.libro = libro
+        #self.socio = socio
+
+    def registrar_prestamo(self, fecha: str):
+        print(f"Prestamo registrado: Libro {self.libro.titulo}, Libro {self.libro.nombre}, Fecha: {self.fecha}")
+        self.libro.prestar()
+
+    def devolver_prestamo(self):
+        print(f"Devolucion de Prestamo: Libro {self.libro.titulo}, Libro {self.libro.nombre}")
+        self.libro.devolver()
+
+    def informacion(self):
+        print(f"Prestamo del libro {self.libro.titulo} al libro {self.libro.nombre} {self.libro.apellido}")

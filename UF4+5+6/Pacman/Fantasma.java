0f60d4d12ea46df4762561e0750cc3c49cfccc55
Ikerm01 <ikermacias10@gmail.com>
2024-04-26 09:08:57 +0200

    He creado la classe de fantasma y he puesto los atributos, lo siguiente sera poner las operaciones

---
+++ UF4+5+6/Pacman/Fantasma.java
@@ -0,0 +1,13 @@
+package Pacman;
+
+public class Fantasma {
+    private int posX;
+    private int posY;
+    private String nom;
+
+    public Fantasma(int posX, int posY, String nom){
+        this.posX = posX;
+        this.posY = posY;
+        this.nom = nom;
+    }    
+}

211fa2f1a5d51f7c7a6adb6e05ea8c715644a82b
Ikerm01 <ikermacias10@gmail.com>
2024-05-02 08:27:08 +0200

    He creado el package y he creado la clase de gestor notes y los dos constructores

---
+++ UF4+5+6/Gestió_de_notes/GestorNotes.java
@@ -0,0 +1,14 @@
+package Gestió_de_notes;
+
+class GestorNotes {
+    String nom_alumne;
+    double notes;
+
+    public void registrar_notes(String nom_alumne, double[] notes){
+
+    }
+    
+    public void obtenir_nota(String nom_alumne){
+        
+    }
+}

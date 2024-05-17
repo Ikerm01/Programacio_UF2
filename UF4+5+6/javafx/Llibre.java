1e2cf130f113b6f5da724775b06084748c2eb92a
Ikerm01 <151910000+Ikerm01@users.noreply.github.com>
2024-05-17 16:22:03 +0200

    Llibre.java

---
+++ UF4+5+6/javafx/Llibre.java
@@ -0,0 +1,16 @@
+public class Llibre {
+    private String titol;
+    private String autor;
+
+    public Llibre(String titol, String autor) {
+        this.titol = titol;
+        this.autor = autor;
+    }
+
+    public String getTitol() {
+        return titol;
+    }
+
+    public String getAutor() {
+        return autor;
+    }

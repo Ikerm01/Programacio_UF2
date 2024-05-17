028459155a1f7374a777ee426d2e712133584bef
Ikerm01 <ikermacias10@gmail.com>
2024-05-17 09:18:08 +0200

    He añadido a la clase de task la descripcion y la fecha de vencimiento y ademas he modificado el constructor

---
+++ UF4+5+6/Gestor_tasques/Taskmanager.java
@@ -0,0 +1,117 @@
+package Gestor_tasques;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+class TaskManagement
+{
+	HashMap<String, ArrayList<Task>> lists;
+	HashMap<Integer, Task> tasks;
+
+	public TaskManagement() {
+		lists = new HashMap<String, ArrayList<Task>>();
+		lists.put("Baja", new ArrayList<Task>());
+		lists.put("Media", new ArrayList<Task>());
+		lists.put("Alta", new ArrayList<Task>());
+		tasks = new HashMap<Integer, Task>();
+	}
+
+	public void addTask(String taskName, String priority) {
+		Task task = new Task(taskName);
+		lists.get(priority).add(task);
+		tasks.put(task.getId(), task);
+	}
+
+	public void doneTask(int taskId) {
+		tasks.get(taskId).done();
+	}
+
+	public void deleteTask(int taskId) {
+		tasks.remove(taskId);
+	}
+
+	public void printAllTask() {
+		tasks.forEach((key, task) -> {
+			System.out.println(task);
+		}
+		);
+	}
+
+	public void printTaskByPriority(String priority) {
+		System.out.println("Tareas de prioridad: " + priority);
+		for (Task task: lists.get(priority)) {
+			System.out.println(task);
+		}
+	}
+    
+}
+
+class Task {
+    private static int numInstances = 0;
+    private int id;
+    private String name;
+    private String description;
+    private String dueDate;
+    private boolean isDone;
+    private String status; 
+
+    public Task(String name, String description, String dueDate) {
+        this.name = name;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.isDone = false;
+         // Inicialmente el estado es "Pendiente"
+        this.status = "Pendiente";
+        this.id = ++numInstances;
+    }
+
+    public void done() {
+        this.isDone = true;
+         // Actualizar el estado de la tarea
+        this.status = "Completada"; 
+    }
+
+    public int getId() {
+        return this.id;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getDueDate() {
+        return this.dueDate;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public boolean isDone() {
+        return this.isDone;
+    }
+}
+
+class Main
+{
+	public static void main(String[] args) {
+		TaskManagement manager = new TaskManagement();
+		System.out.println("Add task");
+		manager.addTask("Aprobar Programacion", "Alta");
+		System.out.println("Hacemos Print de todo");
+		manager.printAllTask();
+		System.out.println("Hacemos Print de prioridad");
+		manager.printTaskByPriority("Alta");
+		System.out.println("Hacemos que la tarea este done");
+		manager.doneTask(1);
+		System.out.println("Hacemos Print de todo");
+		manager.printAllTask();
+		System.out.println("Eliminamos la tarea");
+		manager.deleteTask(1);
+		System.out.println("Hacemos Print de todo");
+		manager.printAllTask();
+	}
+}

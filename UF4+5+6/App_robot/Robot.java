16c8d76c7ec33be166af1cc4c30fd6b30d4f3b1f
Ikerm01 <ikermacias10@gmail.com>
2024-05-24 08:46:46 +0200

    He añadido la configuracion para ventana

--- UF4+5+6/App_robot/Robot.java
+++ UF4+5+6/App_robot/Robot.java
@@ -4,8 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 
 class Robot {
-    private JFrame frame /*Ventana */
-    private JPanel panel /*Panel para meter los botones */
+    private JFrame frame; /*Ventana */
+    private JPanel panel; /*Panel para meter los botones */
     private JButton Moure_Endevant, Moure_Enrere, Girar_Esquerra, Girar_Dreta, Guardar_Estat, Carregar_Estat; /*Botones */
 
+
+    public Robot(){
+        robot = new Robot();
+        /*Configuracion de ventana */
+        frame = new JFrame("Control de Robot");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,300); 
+
+
+    }
 }

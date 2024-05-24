d712beca2dbeae84b3dd4a81bee4f27ea6d2dc2e
Ikerm01 <ikermacias10@gmail.com>
2024-05-24 08:41:37 +0200

    He añadido los componentes de la interfaz grafica

--- UF4+5+6/App_robot/Robot.java
+++ UF4+5+6/App_robot/Robot.java
@@ -4,5 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 
 class Robot {
-    
+    private JFrame frame /*Ventana */
+    private JPanel panel /*Panel para meter los botones */
+    private JButton Moure_Endevant, Moure_Enrere, Girar_Esquerra, Girar_Dreta, Guardar_Estat, Carregar_Estat; /*Botones */
+
 }
